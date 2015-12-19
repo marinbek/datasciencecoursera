7b7522cd4810ce29f58d13188c385abc44c37ae1
Marin Bek <marin.bek@gmail.com>
2015-12-19 18:10:19 +0100

    comments

--- r_programing/week2/corr.R
+++ r_programing/week2/corr.R
@@ -5,14 +5,19 @@ corr <- function(directory, threshold = 0) {
   # go over all requested measurement files
   for (filename in list.files(directory, pattern="*.csv")) {
     
+    # use sprintf to join directory name and file name
     full_path <- sprintf("%s/%s", directory, filename)
     data <- read.csv(full_path)
     
+    # get vector of complete cases, skip if there are no complete cases or 
+    # if the threshold is not met
     complete_cases <- sum(complete.cases(data))
     if (complete_cases < threshold || complete_cases == 0) {
       next
     }
     
+    # calculate correlation using 'cor' and append to result vector
+    # "use=complete.obs" means it will skip rows where data is not complete
     correlations <- c(correlations, cor(data[,"nitrate"], data[,"sulfate"], use="complete.obs"))
   }
 
